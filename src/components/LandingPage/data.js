7a1b022d2421d8b8ccac384afc36e07d63d12b5b
Gray <btgray6@gmail.com>
2022-01-14 15:01:55 +0000

    my bit

---
+++ src/components/LandingPage/data.js
@@ -0,0 +1,11 @@
+export const data = [
+    {count:"1",name:"Bulbasaur",color:"82B6B37e"},
+    {count:"2",name:"Ivysaur",color:"82B6B37e"},
+    {count:"3",name:"Venusaur",color:"82B6B37e"},
+    {count:"4",name:"Charmander",color:"EC9F5E7e"},
+    {count:"5",name:"Charmeleon",color:"EC9F5E7e"},
+    {count:"6",name:"Charizard",color:"EC9F5E7e"},
+    {count:"7",name:"Squirtle",color:"90C6D37e"},
+    {count:"8",name:"Wartortle",color:"90C6D37e"},
+    {count:"9",name:"Blastoise",color:"90C6D37e"},
+]
